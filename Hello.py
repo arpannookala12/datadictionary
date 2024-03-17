8abac193e507e2668b84df3a1e9ac8cad69ee72b
arpannookala12 <157387187+arpannookala12@users.noreply.github.com>
2024-03-17 17:46:31 +0000

    first

--- Hello.py
+++ Hello.py
@@ -14,11 +14,12 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
-
+import pandas as pd
 LOGGER = get_logger(__name__)
 
 
 def run():
+    df = pd.read_csv("CDC_metadata.csv")
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
